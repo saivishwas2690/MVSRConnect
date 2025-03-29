c0d6e7a219246690789b207a6ade3f62ba3c375d
saivishwas <saivishwas2690@gmail.com>
2025-03-29 15:57:13 +0530

    updated settings.py

--- MVSRConnect/settings.py
+++ MVSRConnect/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DB_NAME", "False").lower() == "true"
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
